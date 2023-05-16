a2733f5348be3ebc6d2dabbab6ecd411d5bc9c52
Igor Steffano <igor.viana1@live.com>
2023-05-16 16:14:41 -0300

    Create cypress project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -120,7 +120,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       })
     })
 
-    it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
         cy.get('#privacy a').should('have.attr', 'target', '_blank' )
     })
     
